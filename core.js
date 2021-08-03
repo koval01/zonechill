8eb5722cea46c6acbe0f8ab3080c31651f0228f3
Koval Yaroslav <koval@q-writer.com>
2021-08-03 21:48:44 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,39 +1,43 @@
-document.querySelector(document).ready(function(){
-  var n=0;
-  setInterval(increment,1000);
-  function increment(){
-    n++;
-    setCounter(n);
-  }
-  function setCounter(v){
-    var counter=document.querySelector(".counter");
-    var old=counter.children(".counter-value");
-    var oldContent=old.children(".counter-value-mask");
+(function() {
+    var n=0;
 
-    var t=0.4;
-    var d=t*0.0;
-    var d2=t*0.3;
-    var padding=55;
-    var offset=5;
-    var w=old.data("w");
-    w+=padding;
-    TweenMax.to(old,t,{delay:d,x:w,ease:Quad.easeIn});
-    TweenMax.to(oldContent,t,{delay:d,x:-(w-offset),ease:Quad.easeIn});
-    setTimeout(function(){old.remove()},t*1000);
-    
-    var neu=document.querySelector("<div/>").classList.add("counter-value").appendTo(counter);
-    var neuContent=document.querySelector("<div/>").classList.add("counter-value-mask").appendTo(neu).text(v);
-    
-    w=neuContent.width();
-    neu.data("w",w);
-    neu.css({
-      width:w
-    })
-    w+=padding;
-    TweenMax.from(neu,t,{delay:d2,x:-w});
-    TweenMax.from(neuContent,t,{delay:d2,x:w-offset});
-    
-    
-    
-  }
-})
+    setInterval(increment,1000);
+
+    function increment(){
+        n++;
+        setCounter(n);
+    }
+
+    function setCounter(v){
+        let counter=document.querySelector(".counter");
+        let old=counter.children(".counter-value");
+        let oldContent=old.children(".counter-value-mask");
+
+        let t=0.4;
+        let d=t*0.0;
+        let d2=t*0.3;
+        let padding=55;
+        let offset=5;
+        let w=old.data("w");
+
+        w+=padding;
+        TweenMax.to(old,t,{delay:d,x:w,ease:Quad.easeIn});
+        TweenMax.to(oldContent,t,{delay:d,x:-(w-offset),ease:Quad.easeIn});
+
+
+        setTimeout(function(){old.remove()},t*1000);
+        
+        var neu=document.querySelector("<div/>").classList.add("counter-value").appendTo(counter);
+        var neuContent=document.querySelector("<div/>").classList.add("counter-value-mask").appendTo(neu).text(v);
+        
+        w=neuContent.width();
+        neu.data("w",w);
+        neu.css({
+            width:w
+        })
+
+        w+=padding;
+        TweenMax.from(neu,t,{delay:d2,x:-w});
+        TweenMax.from(neuContent,t,{delay:d2,x:w-offset});
+    }
+})();
