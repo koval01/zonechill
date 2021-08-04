c6cdd01f416fddd3355272b8637fa240d80e08c7
Koval Yaroslav <koval@q-writer.com>
2021-08-04 12:06:09 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,16 +1,76 @@
-$(document).ready(function(){
+$(document).ready(function() {
     const api_url = "https://api.telegram.org/bot";
-    const token = "1928555652:AAGk3oYaHlx-MLLnclv8T2TVtJhlTXX9oJc"
+    const file_api_url = "https://api.telegram.org/file/bot";
+    const token = "1928555652:AAGk3oYaHlx-MLLnclv8T2TVtJhlTXX9oJc";
+    const channel_uname = "zonechill";
 
     var n=0;
 
     const req_url =  api_url+token;
+    const f_req_url = file_api_url+token;
 
     setInterval(increment, 1000);
+    
+    function get_channel() {
+        const method = "getChat";
+        
+        $.ajax({
+            url: `${req_url}/${method}`,
+            type: "GET",
+            data: {"chat_id": `@${channel_uname}`},
 
-    function increment(){
+            success: function (o) {
+                return o;
+            },
+
+            error: function () {
+                console.log("Error! Failed to query Telegram API and get channel information.")
+            },
+        });
+    }
+    
+    function get_channel_photo_path(photo_file_id) {
+        const method = "getFile";
+        
+        $.ajax({
+            url: `${req_url}/${method}`,
+            type: "GET",
+            data: {"file_id": photo_file_id},
+
+            success: function (o) {
+                return o['file_path'];
+            },
+
+            error: function () {
+                console.log("Error! Failed to query Telegram API and get channel information.")
+            },
+        });
+    }
+    
+    function get_channel_photo(photo_path) {
+        return `${f_req_url}/${photo_path}`;
+    }
+    
+    function image_set__(path_) {
+        $(".image_channel__").attr("src", path_);
+    }
+    
+    function channel_image() {
+        let channel = get_channel();
+        
+        if (channel["ok"]) {
+            let id_ = channel["result"]["photo"]["big_file_id"];
+            let path_ = get_channel_photo_path(id_);
+            
+            // setting image to default img container
+            image_set__(get_channel_photo(path_));
+        } else {
+            console.log("Error get channel json data!");
+        }
+    }
+
+    function increment() {
         const method = "getChatMemberCount";
-        const channel_uname = "zonechill";
 
         $.ajax({
             url: `${req_url}/${method}`,
@@ -30,7 +90,7 @@ $(document).ready(function(){
         });
     }
 
-    function setCounter(v){
+    function setCounter(v) {
         var counter=$(".counter");
         var old=counter.children(".counter-value");
         var oldContent=old.children(".counter-value-mask");
@@ -63,4 +123,8 @@ $(document).ready(function(){
         TweenMax.from(neu,t,{delay:d2,x:-w});
         TweenMax.from(neuContent,t,{delay:d2,x:w-offset});
     }
+    
+    // ready call
+    channel_image();
+    
   })
