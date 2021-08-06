f07b42cbd93858514854060d773c281c3429da96
Koval Yaroslav <koval@q-writer.com>
2021-08-06 20:10:44 +0300

    Add files via upload

--- core.js
+++ core.js
@@ -4,30 +4,250 @@ $(document).ready(function() {
     const token = "1928555652:AAGk3oYaHlx-MLLnclv8T2TVtJhlTXX9oJc";
     const channel_uname = "zonechill";
 
+    const yt_tokens = [
+        "AIzaSyCko11Nnujt1K-YPEhwYAgp3S6IpsIuxeA",
+        "AIzaSyAEnbBY3SPdwImzX5gek4djK7mrNNd-fow",
+    ];
+    const yt_api_url = "https://www.googleapis.com/youtube/v3";
+    const yt_channel_elza = "UCUEoKLBj-lelT29QAEkkg2A";
+
     var channel_name__ = "";
     var s_mem = 0;
+
+    const pages_num = $("body").html().match(/page_*\d?/g).length;
 
     const req_url = api_url + token;
     const f_req_url = file_api_url + token;
 
-    setInterval(increment, 1000);
+    setInterval(increment, 1000);    
+
+    function NumbersFormatter(num, digits) {
+        const lookup = [
+            { value: 1, symbol: "" },
+            { value: 1e3, symbol: "k" },
+            { value: 1e6, symbol: "M" },
+            { value: 1e9, symbol: "G" },
+            { value: 1e12, symbol: "T" },
+            { value: 1e15, symbol: "P" },
+            { value: 1e18, symbol: "E" }
+        ];
+
+        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var item = lookup.slice().reverse().find(function(item) {
+            return num >= item.value;
+        });
+
+        return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+    }
     
     function numberPretify(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     }
-    
-    function api_status_set(success = false) {
-        let status = "";
-        let auth__ = "Developed by <a href=\"https://t.me/koval_yaroslav\" target=\"_blank\">Koval Yaroslav</a>";
-        let git_ = "Source code on <a href=\"https://github.com/koval01/zonechill\" target=\"_blank\">GitHub</a>";
+
+    function move_to_page(page_int, page_hash = null) {
+        if (page_int) {
+            page_int = parseInt(page_int);
+        } else if (page_hash) {
+            page_int = $(page_hash.replace("#", ".")).attr("id").replace(/[^+\d]/g, "");
+        }
+        const page_ = $("#page_"+page_int);
+
+        location.hash = page_.attr("class");
+
+        page_.css("display", "block");
         
+        for (let i = 0; i < pages_num; i++) {
+            if (i != page_int) {
+                $("#page_"+i).css("display", "none");
+            }
+        }
+
+        /*
+        Просто и понятно
+        Ненужные контейнеры отключаем, а нужный включаем
+        */
+    }
+    
+    function api_tg_status_set(success = false) {
         if (success) {
             status = "OK";
         } else {
             status = "ERROR";
         }
         
-        $(".status_api_text").html(`Telegram API: ${status} | ${auth__} | ${git_}`);
+        $("#telegram_api_status_").html(`Telegram API: ${status}`);
+    }
+
+    function api_yt_status_set(success = false) {
+        if (success) {
+            status = "OK";
+        } else {
+            status = "ERROR";
+        }
+        
+        $("#youtube_api_status_").html(`YouTube API: ${status}`);
+    }
+
+    function get_channel_data_yt(callback) {
+        const method = "channels";
+
+        var make_request_ = function() {
+            $.ajax({
+                url: `${yt_api_url}/${method}`,
+                type: "GET",
+                data: {
+                    "part": "statistics",
+                    "key": yt_tokens[Math.floor(Math.random()*yt_tokens.length)],
+                    "id": yt_channel_elza,
+                },
+
+                success: function(o) {
+                    callback(o);
+                    api_yt_status_set(true);
+                },
+
+                error: function() {
+                    console.log("Error! Could not get channel data from YouTube.");
+                    api_yt_status_set();
+                    make_request_();
+                },
+            });
+        }
+        make_request_();
+    }
+
+    function get_video_yt_stat(callback, video_id) {
+        const method = "videos";
+
+        var make_request_ = function() {
+            $.ajax({
+                url: `${yt_api_url}/${method}`,
+                type: "GET",
+                data: {
+                    "part": "statistics",
+                    "key": yt_tokens[Math.floor(Math.random()*yt_tokens.length)],
+                    "id": video_id,
+                },
+
+                success: function(o) {
+                    callback(o["items"][0]["statistics"]);
+                    api_yt_status_set(true);
+                },
+
+                error: function() {
+                    console.log("Error! Could not get video statistics from YouTube.");
+                    api_yt_status_set();
+                    make_request_();
+                },
+            });
+        }
+        make_request_();
+    }
+
+    function get_last_videos_yt(callback) {
+        const method = "search";
+
+        var make_request_ = function() {
+            $.ajax({
+                url: `${yt_api_url}/${method}`,
+                type: "GET",
+                data: {
+                    "part": "snippet",
+                    "channelId": yt_channel_elza,
+                    "maxResults": 10,
+                    "order": "date",
+                    "type": "video",
+                    "key": yt_tokens[Math.floor(Math.random()*yt_tokens.length)],
+                },
+
+                success: function(o) {
+                    const items = o['items'];
+                    const stat = o['statistics'];
+                    var array_videos = [];
+
+                    for (let i = 0; i < items.length; i++) {
+                        const snippet_ = items[i]["snippet"];
+
+                        array_videos.push({
+                            "video_id": items[i]["id"]["videoId"],
+                            "video_title": snippet_["title"],
+                            "video_description": snippet_["description"],
+                            "channel_title": snippet_["channelTitle"],
+                            "channel_id": snippet_["channelId"],
+                        });
+                    }
+
+                    callback(array_videos);
+
+                    api_yt_status_set(true);
+                },
+
+                error: function(r) {
+                    console.log("Error! Could not get video list from YouTube.");
+                    // $("#youtube_videos_container_").append("<p class=\"error_api_text\">Произошла ошибка! Не удалось выполнить запрос к API.</p>");
+                    api_yt_status_set();
+                    make_request_();
+                },
+            });
+        }
+        make_request_();
+    }
+
+    function set_last_video_yt() {
+        get_last_videos_yt(function(data) {
+            if (data.length) {
+                for (let i = 0; i < data.length; i++) {
+                    const array_ = data[i];
+
+                    get_video_yt_stat(function(data) {
+                        const template_video_block = `
+                        <div class="col">
+                            <div class="card shadow-sm">
+                            <div class="video_preview_yt">
+                                <iframe width="100%" style="display:block;margin:auto;height:40%" src="https://www.youtube.com/embed/${array_['video_id']}"
+                                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+                                    encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                                </iframe>
+                            </div>
+                
+                            <div class="card-body">
+                                <p class="card-text video_title_yt">${array_['video_title']}</p>
+                                <p class="card-text video_description_yt" id="yt_desc_${i}">${array_['video_description']}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                <small class="text-muted">
+                                    <a href="https://www.youtube.com/channel/${array_["channel_id"]}" class="channel_link_yt" target="_blank">
+                                        <i class="fab fa-youtube channel_title_icon"></i> ${array_['channel_title']}
+                                    </a>
+                                    <br/>
+                                    <i class="far fa-eye"></i> ${NumbersFormatter(data["viewCount"])} | 
+                                    <i class="far fa-heart"></i> ${NumbersFormatter(data["likeCount"])} |
+                                    <i class="far fa-comments"></i> ${NumbersFormatter(data["commentCount"])}
+                                </small>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        `;
+
+                        $("#youtube_videos_container_").append(template_video_block);
+
+                        $('#yt_desc_'+i).linkify({
+                            target: "_blank"
+                        });
+
+                    }, array_["video_id"]);
+                }
+            } else {
+                // $("#youtube_videos_container_").append("<p class=\"error_api_text\">Произошла ошибка! Не удалось получить массив данных о видео.</p>");
+                // pass
+            }
+        });
+    }
+
+    function channel_yt_set() {
+        get_channel_data_yt(function(data) {
+            console.log(data);
+        });
     }
 
     function get_channel(callback) {
@@ -43,16 +263,16 @@ $(document).ready(function() {
             success: function(o) {
                 if (o["ok"]) {
                     callback(o["result"]);
-                    api_status_set(true);
+                    api_tg_status_set(true);
                 } else {
                     console.log("Error get channel json data!");
-                    api_status_set();
+                    api_tg_status_set();
                 }
             },
 
             error: function() {
                 console.log("Error! Failed to query Telegram API and get channel information.");
-                api_status_set();
+                api_tg_status_set();
             },
         });
     }
@@ -70,12 +290,12 @@ $(document).ready(function() {
             success: function(o) {
                 let path = o["result"]["file_path"];
                 callback(path);
-                api_status_set(true);
+                api_tg_status_set(true);
             },
 
             error: function() {
                 console.log("Error! Failed to query Telegram API and get channel information.");
-                api_status_set();
+                api_tg_status_set();
             },
         });
     }
@@ -122,6 +342,17 @@ $(document).ready(function() {
                 console.log("Not found pinned messages!");
                 callback(null);
             }
+        });
+    }
+
+    function set_pin_comments() {
+        get_channel(function(data) {
+            const pin = data["pinned_message"]['message_id'];
+
+            $(".comments_block_box").html(`
+            <script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-discussion="zonechill/${pin}" 
+            data-comments-limit="15" data-colorful="1" data-color="343638" data-dark="1" data-dark-color="FFFFFF"></script>
+            `);
         });
     }
  
@@ -181,12 +412,12 @@ $(document).ready(function() {
                     $("title").text(`${channel_name__} - ${count_} SUBS`);
                 }    
                 
-                api_status_set(true);
+                api_tg_status_set(true);
             },
 
             error: function() {
                 console.log("Error! Failed to query Telegram API and get channel information.");
-                api_status_set();
+                api_tg_status_set();
             },
         });
     }
@@ -195,6 +426,11 @@ $(document).ready(function() {
         channel_image();
         channel_name();
         channel_desc();
+
+        set_pin_comments();
+
+        set_last_video_yt();
+        channel_yt_set();
 
         create_pinned_message();
         setInterval(create_pinned_message(), 5000);
@@ -206,7 +442,20 @@ $(document).ready(function() {
         counter.text(v);
     }
 
+    $("body").click(function(event) {
+        let obj_id = event.target.id;
+        let id_int = obj_id.replace(/[^+\d]/g, "");
+
+        if (obj_id.indexOf("#page_") && id_int) {
+            move_to_page(id_int);
+        }
+    });
+
     // ready call
     call_();
+
+    if (location.hash) {
+        move_to_page(false, location.hash);
+    }
 
 })
